72635232b533e26a89679593ad007590b9caa695
Whe1ve <zhangruijin666888@gmail.com>
2023-12-14 23:33:32 +0800

    update on 12/14

--- source/myShell.c
+++ source/myShell.c
@@ -2,10 +2,204 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <readline/readline.h>
+#include <dirent.h>
 
+//help command
+void help(int argc,char**argv){
+   //show all inner command
+   if(argc==1){
+       printf("A simple shell implementation by whe1ve @version 1.0\n");
+       printf("These shell commands are defined internally. Type 'help' to see the list.\n");
+       printf("Type 'help name' to find out more about the function 'name'.\n");
+       printf("cd\t[dir]\n");
+       printf("exit\t[n]\n");
+       printf("help\t[patten]\n");
+       printf("mv \t[source]\t[destination]\n");
+       printf("rm \t[file|[[-r] [dir]]]\n");
+   }
+   else{
+      if(strcmp(argv[1],"cd")==0){
+        //show 'cd' command's detail information
+        printf("cd:\tcd [dir]\n");
+        printf("\tChange the shell working directory.\n");
+        printf("\n");
+        printf("\tChange the current directory to DIR. The default DIR is the value of the HOME shell variable.\n");
+      }
+      else if(strcmp(argv[1],"exit")==0){
+        //show 'exit' command's detail information
+        printf("exit:\texit [n]\n");
+        printf("\tExit the shell.\n");
+        printf("\n");
+        printf("\tExits the shell with a status of N.  If N is omitted, the exit status is 0.\n");
+      }
+      else if(strcmp(argv[1],"help")==0){
+        //show 'help' command's detail information
+        printf("help:\thelp [pattern]\n");
+        printf("\tDisplay information about builtin commands.\n");
+        printf("\n");
+        printf("\tDisplays brief summaries of builtin commands. If PATTERN is specified, gives detailed help on all commands matching PATTERN, otherwise the list of help topics is printed.\n");
+      }
+      else if(strcmp(argv[1],"mv")==0){
+        //show 'mv' command's detail information
+        printf("mv:\tmv [source] [destination]\n");
+        printf("\tMove the source file to the destination diretory.\n");
+        printf("\n");
+        printf("\tIf the source and destination are in the same directory, the rename the source file.\n");
+      } 
+      else if(strcmp(argv[1],"rm")==0){
+        //show 'rm' command's detail information
+        printf("rm:\trm [fire|[[-r] [dir]]]\n");
+        printf("\tRemove file or remove the directory recursively.\n");
+      }
+      else{
+          perror("Error:no such inner command.\n");
+          return;
+      }
+   }
+}
+
+//cd command
+void cd(int argc,char**argv){
+    if(argc==1){
+        //change the current working directory to home directory.
+        char *homeDir = getenv("HOME");
+        chdir(homeDir);
+    }
+    else if(argc==2){
+        //change the current working directory to higher level directory
+        if(strcmp(argv[1],"..")==0){
+            chdir("..");
+        }
+        else{
+            chdir(argv[1]);
+        }
+    }
+    else if(argc>2){
+        perror("Error:too many arguments.\n");
+    }
+}
+
+//remove directory recursively
+int removeDirectory(const char *dirPath) {
+    DIR *dir;
+    struct dirent *entry;
+    char path[1024];
+
+    if ((dir = opendir(dirPath)) == NULL) {
+        perror("Error:can not open the directory.\n");
+        return 1;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+            snprintf(path, sizeof(path), "%s/%s", dirPath, entry->d_name);
+
+            if (entry->d_type == DT_DIR) {
+                // remove sub-directory recursively
+                if (removeDirectory(path) != 0) {
+                    closedir(dir);
+                    return 1;
+                }
+            } 
+            else {
+                // remove the file
+                if (remove(path) != 0) {
+                    perror("Error:can not remove the file.\n");
+                    closedir(dir);
+                    return 1;
+                }
+            }
+        }
+    }
+
+    closedir(dir);
+
+    // remove empty directory
+    if (rmdir(dirPath) == 0) {
+            return 0;
+    } 
+    else {
+        perror("Error:can not remove the directory.\n");
+        return 1;
+    }
+}
+
+//rm command
+void rm(int argc,char**argv){
+    if(argc==2){
+        //remove the file
+        if(opendir(argv[1])!=NULL){
+            perror("Error:can not remove the directory that is not empty.\n");
+            return;
+        }
+        if(remove(argv[1])!=0){
+            perror("Error: the file is not exist or cannot remove it.\n");
+            return;
+        }
+    }
+    else{
+        if(argc==1){
+            //missing operand
+            perror("Error: missing operand!\n");
+            return;
+        }
+        else if(argc>3){
+            //too many arguments
+            perror("Error: too many arguments.\n");
+            return;
+        }
+        else if(argc==3){
+
+            if(strcmp(argv[1],"-r")!=0){
+                perror("Error: wrong option.\n");
+                return;
+            }
+            //remove the directory recursively
+            if(removeDirectory(argv[2])){
+                perror("Error:remove failed.\n");
+            }
+
+        }
+    }
+}
+
+//mv command
+void mv(int argc,char**argv){
+    if(argc<3){
+        perror("Error: incomplete command.\n");
+        return;
+    }
+    else if(argc>3){
+        perror("Error: too many arguments.\n");
+        return;
+    }
+    else{
+        //rename or move the file
+        if(opendir(argv[2])==NULL){
+            //rename the file
+            rename(argv[1], argv[2]);
+            return;
+        }
+        else{
+            //move the file to the target directory
+            char destinationPath[1024];
+            //integrate the destination path
+            snprintf(destinationPath, sizeof(destinationPath), "%s/%s", argv[2], argv[1]);
+            if(rename(argv[1],destinationPath)!=0){
+                perror("Error: can not move the file.\n");
+                return;
+            }
+        }
+    }
+}
+
+//ls command
+void ls(int argc,char*argv){
+
+}
 
 //Out put the command line prompt
-void print_prompt(){
+void print_prompt(char*prompt){
     char *user_name = getlogin(); //the user nmae 
     char host_name[256];
     gethostname(host_name, sizeof(host_name)); //get host name
@@ -21,15 +215,19 @@ void print_prompt(){
         //non-root user
         userType = '$';
     }
-    printf("\033[0;35m%s@%s:%s%c\033[0m", user_name, host_name, cwd, userType);//print the command line prompt
+
+    sprintf(prompt, "\033[0;35m%s@%s:%s%c \033[0m", user_name, host_name, cwd, userType);
+    
 }
 //get user's command
 void get_command(int argc,char**argv,char*command){
 
     const char delimiter = ' ';
+    char temp[1024];
+    strcpy(temp, command);
     for (int j = 0; j < (argc);++j){
         if(j==0){
-            strcpy(argv[0], strtok(command, &delimiter));
+            strcpy(argv[0], strtok(temp, &delimiter));
             //printf("%s\n", argv[0]);
         }
         else{
@@ -40,21 +238,71 @@ void get_command(int argc,char**argv,char*command){
 }
 
 //deal the command
-void deal_command(){
+void deal_command(int argc,char**argv,char*command){
+   //exit command
+   if(strcmp(argv[0],"exit")==0){
+       if(argc==1){
+           exit(0);
+       }
+       else if(argc==2){
+           exit(atoi(argv[1]));
+       }
+       else{
+           perror("Error:invalid command\n");
+           return;
+       }
+   }
 
+   //help command
+   if(strcmp(argv[0],"help")==0){
+       help(argc,argv);
+       return;
+   }
+
+   //cd command
+   if(strcmp(argv[0],"cd")==0){
+       cd(argc, argv);
+       return;
+   }
+
+   //rm command
+   if(strcmp(argv[0],"rm")==0){
+       rm(argc, argv);
+       return;
+   }
+   
+
+   //mv command
+   if(strcmp(argv[0],"mv")==0){
+       mv(argc, argv);
+       return;
+   }
+   
+   //ls command
+   if(strcmp(argv[0],"ls")==0){
+       ls(argc, argv);
+   }
+   system(command);
 }
+
+
 int main(){
 
     while(1){
         char *command; // User input
-        
-        print_prompt();// display the command line prompt
+        char prompt[1024]; //prompt info
+        print_prompt(prompt);// display the command line prompt
+        rl_save_prompt();//save prompt info
+
 
         int argc = 0;
         char **argv;
         const char delimiter = ' ';
 
-        command = readline("");
+        command = readline(prompt);
+        if(strcmp(command,"\n")==0){
+            continue;
+        }
         char temp[4096];
         strcpy(temp, command);        
         char *t=strtok(temp,&delimiter);
@@ -71,13 +319,14 @@ int main(){
 
         get_command(argc, argv, command); // get stoken
 
+        deal_command(argc, argv,command); //deal command
 
         //free the memory
         for (int i = 0; i < argc;++i){
             free(argv[i]);
         }
         free(argv);
-        break;
+        free(command);
     }
     return 0;
 }
